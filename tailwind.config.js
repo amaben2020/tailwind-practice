dd1059a8e4da91323e81fa2f0ce9ce0006207118
algomachine007 <benneth@bejamas.io>
2022-09-19 20:53:43 +0100

    extending styles and functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  mode: 'jit',
+  darkMode: 'class',
   theme: {
-    extend: {},
+    // custom breakpoints
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      // custom breakpoints
+      colors: {
+        brightRed: 'hsl(12,88%, 59%',
+        brightRedLight: 'hsl(12,88%, 69%',
+        brightRedSupLight: 'hsl(12,88%, 95%',
+      },
+    },
   },
   plugins: [],
 };
